6afd6718cc3ed9a3210864020428608531f6ad0d
Pranoj Ranjit <pra17jit@gmail.com>
2023-10-30 23:20:55 -0400

    Initial Commit

---
+++ Team12_MAPD714_Milestone2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team12_MAPD714_Milestone2
+//
+//  Created by Pranoj Ranjit on 2023-10-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
